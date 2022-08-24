d7a43e7c9430d1da272c5c0c9722a0d1b1edf17a
saidmoussadek <saiddus24@gmail.com>
2022-08-24 09:22:13 +0200

    correzione

--- js/main.js
+++ js/main.js
@@ -2,14 +2,17 @@ var app = new Vue({
     el: '#app',
     data: {
       //variabile ce conterra la risposta
-      emailCasuale: null
+      emailCasuale: []
     },
     mounted(){
-       
+      
+       for (let i=0; i<10; i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then(response => {
-                this.emailCasuale = response.data.response;
-            })
+                this.emailCasuale.push (response.data.response)
+                
+           });
+           } 
         }
 
   })
